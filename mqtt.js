3fea45315382ea6fd5810a3508b7f65b579f59e6
nimbusdin <40475930+nimbusdin@users.noreply.github.com>
2019-04-11 21:32:39 -0500

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -2,8 +2,18 @@ function mqtt(req, resp) {
   var val = req.params.lscpu;
   ClearBlade.init({request:req});
   var collection = ClearBlade.Collection({collectionName:"mqtt"});
+  //resp.success(val.length);
+  specs = {};
+  for (i = 0; i < val.length; i++) {
+    col_name = val[i]["field"];
+    col_name = col_name.replace(/ /g, '_');
+    col_name = col_name.replace(/[()-:]/g, '');
+    collection.addColumn(col_name);
+    col_value = val[i]["data"];
+    specs[col_name] = col_value
+  }
   var newRow = {
-    lscpu: JSON.stringify(val)
+    lscpu: JSON.stringify(specs)
   };
   collection.create(newRow);
   resp.success(JSON.stringify(newRow));
